21ecb830022fdfce943f413156fa6b1834fa4676
liujinhou <www.975764577@qq.com>
2017-03-09 22:55:55 +0800

    Author: Adam Funtion:The First Descriton: NONE

---
+++ web/src/main/java/com/jhart/controller/Application.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.jhart.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Adam
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
